070d58ddf81009c7f98d376d04309b403a22088c
muhseenat <mucktd777@gmail.com>
2022-04-09 18:06:08 +0530

    minor changes

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const user = require('../Tasker_Server/src/helpers/user');
 
 const io = require('socket.io')(8900, {
     cors: {
@@ -8,24 +7,41 @@ const io = require('socket.io')(8900, {
 
 let users = []
 
-//Function to avoid duplicate users while pushing to arraya
+//Function to avoid duplicate users while pushing to array and add user
 const addUser = (userId, socketId) => {
     !users.some((user) => user.userId === userId) &&
         users.push({ userId, socketId })
 };
-//Function to remove user
+//Function to remove user when disconnect
 const removeUser = (socketId) => {
     users = users.filter(user => user.socketId !== socketId)
+}
+
+//Function to get users
+const getUser=(userId)=>{
+    return users.find(user=>user.userId === userId);
 }
 
 io.on("connection", (socket) => {
     console.log('a user connected');
     //TAKE USERID AND SOCKETID FROM USER
     socket.on('addUser', (userId) => {
-        addUser(userId, socket.id)
-        io.emit('getUsers', users)
+        addUser(userId, socket.id);
+        io.emit('getUsers', users);
     });
 
+    //Send and get messages
+    socket.on('sendMessage',({senderId,receiverId,text})=>{
+        const user=getUser(receiverId);
+        console.log(user,'ithammm paranajyakkinna user');
+        io.to(user?.socketId).emit('getMessage',{
+            senderId,
+            text,
+        });
+    });
+
+
+    //When disconnect user
     socket.on("disconnect", () => {
         console.log('user removed');
         io.emit('getUsers', users)
